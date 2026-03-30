4e686629e077480977f5083153efb7b8418f6ce7
Azeem <azeem@lm-kv-azeem.local>
2026-03-30 13:11:48 +0530

    fix: Update test db url

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 // Determine the correct connection string based on the environment
 const databaseUrl = process.env.NODE_ENV === 'test'
-    ? (process.env.TEST_DATABASE_URL || 'postgresql://postgres:password@localhost:5433/azerro_test?schema=public')
+    ? process.env.TEST_DATABASE_URL
     : process.env.DATABASE_URL;
 
 // Initialize Prisma with the determined connection URL
